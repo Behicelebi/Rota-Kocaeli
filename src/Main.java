bab9cb9b36b74cad82d3022524d50f61adeb2646
Behicelebi <142922321+Behicelebi@users.noreply.github.com>
2025-03-05 13:40:14 +0300

    jar added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
         //Hello Murat
