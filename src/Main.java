91480855b6bbfec79e0353cb9b29d4da75a6c3ce
Murat Emre Biçici <muratemrebicici@gmail.com>
2025-03-05 15:12:15 +0300

    Class Eklemeleri

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.FileReader;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        //Hello Murat
+        String jsonContent = null;
+        try {
+            jsonContent = new String(Files.readAllBytes(Paths.get("veriseti.json")));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Gson gson = new Gson();
+        AnaVeri anaVeri = gson.fromJson(jsonContent, AnaVeri.class);
+
+        System.out.println("Şehir: " + anaVeri.getCity());
+        System.out.println("Taksi Açılış Ücreti: " + anaVeri.getTaxi().getOpeningFee());
+        System.out.println("Scotter Açılış Ücreti: " + anaVeri.getScotter().getOpeningFee());
+        System.out.println("Martı Tag Açılış Ücreti: " + anaVeri.getMarti_tag().getOpeningFee());
+        System.out.println(anaVeri.getDuraklar().get(0).getId() + " Durağının bir sonraki durağı " + anaVeri.getDuraklar().get(0).getNextStops().get(0).getStopId());
     }
 }
